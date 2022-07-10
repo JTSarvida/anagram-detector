88be71fcc388ac86ea1f4bc2cb7d8c79b0128444
jtsarvida <jtsarvida@gmail.com>
2022-07-09 21:41:53 -0400

    commit tree 0e9e16ce79dffe8ab4e8ffe399eac0fda94cbbc0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        matched_array = []
+        words.each do |item|
+            if item.split("").sort == word.split("").sort
+                matched_array << item
+            else
+                matched_array
+            end
+            
+        end
+        return matched_array
+
+    end
+
+end
